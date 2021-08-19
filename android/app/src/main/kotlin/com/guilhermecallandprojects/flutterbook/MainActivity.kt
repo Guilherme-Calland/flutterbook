ea5c2d82b672536cf57b4e60480e19289fe5f1d1
guilhermow <gui.crb0@gmail.com>
2021-08-19 15:02:56 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/guilhermecallandprojects/flutterbook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guilhermecallandprojects.flutterbook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/gui/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/gui/37b51b90-0796-4ade-88a8-6357f5e54277/home/ubuntu/flutter_projects/flutterbook"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
